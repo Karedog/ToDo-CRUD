4a851377c8caaa544df782d7f75251db32ad99c5
Douglas <douglasdanielx@gmail.com>
2025-12-14 10:48:17 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import router from "./src/routes/routes.js";
 import "dotenv/config"
+import cors from 'cors'
 
 const app = express()
 
 app.use(json())
 app.use(router)
+app.use(cors)
 
 
 app.listen(process.env.PORT, ()=>{
